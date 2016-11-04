05799bf6d9b3d6925367ceacbfb4ed46d5c9a166
Nepal <nepal@net.plm.eds.com>
2016-11-04 10:19:50 -0400

    Changes to be committed: 	modified:   index.html 	modified:   scripts/scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,12 +3,33 @@
 *
 * Description
 */
-angular.module('myApp', [])
-	.controller('DataController', ['$scope', function($scope){
-		$scope.employees = [ {name:'John Doe', address:'Mason, OH'},
-				{name:'Jane Doe', address:'Manhattan, KS'},
-				{name:'Apple Jobs', address:'Milford, OH'}, 
-				{name:'Microsoft Gates', address:'Batavia, OH'},
-				{name:'Amanda Johns', address:'West Chester, OH'}
-				];
-		}]);
+var app = angular.module('myApp', []);
+
+/**
+ Typically, when you create an application you need to setup the initial state for the Angular $scope.
+ You do this by attaching properties to the $scope objects. 
+ The properties contain the view model. All the $scope properties will be available to the template at the point in the DOM where the controller is registered.
+ For more info - https://docs.angularjs.org/guide/controller
+**/
+/**
+The example below demonstrates creating a HelloWorld controller which attaches a greeting property containg the message Hello World to the $scope
+**/
+app.controller('HelloWorld', ['$scope', function($scope){
+	$scope.greeting = "Hello World";
+}]);
+
+
+app.controller('DataController', ['$scope', function($scope){
+	$scope.employees = [ 
+			{name:'John Doe', address:'Mason, OH'},
+			{name:'Jane Doe', address:'Manhattan, KS'},
+			{name:'Apple Jobs', address:'Milford, OH'}, 
+			{name:'Microsoft Gates', address:'Batavia, OH'},
+			{name:'Harold Kumar', address:'London, UK'},
+			{name:'Helmut Kohl', address:'Cologne, Germany'},
+			{name:'Margie Klon', address:'Berlin, Germany'},
+			{name:'Juan Vargas', address:'Mallorca, Spain'}, 
+			{name:'Yuvraj Thapar', address:'Pune, India'},
+			{name:'Gajendra Valiveti', address:'Chennai, India'}
+			];
+	}]);
